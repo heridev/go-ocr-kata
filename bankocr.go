f63c6188f939659259f065f756fc6c1aba1cf7ca
Heriberto Perez <heriberto.perez@crowdint.com>
2014-08-20 16:55:47 -0500

    Completes user story 2, 3 and 4

--- bankocr.go
+++ bankocr.go
@@ -3,8 +3,8 @@ package bankocr
 import (
   "strings"
   "io/ioutil"
+  "strconv"
 )
-
 
 func GetAccountNumber(ocr string) string {
   lines := strings.Split(ocr, "\n")[0:3]
@@ -16,7 +16,7 @@ func GetAccountNumber(ocr string) string {
     for _, val := range lines{
       key = key+val[charNum : charNum+3]
     }
-    accountNumber += OCRMaps[key]
+    accountNumber += getMapValue(key)
     key = ""
     charNum = charNum+3
   }
@@ -24,11 +24,112 @@ func GetAccountNumber(ocr string) string {
   return accountNumber
 }
 
-func getLinesFromFile() []string{
-  fileBytes, _ := ioutil.ReadFile("bankSource.txt")
+func getMapValue(key string) string {
+  mapValue := OCRMaps[key]
+  if mapValue != "" {
+    return mapValue
+  }
+  return "?"
+}
+
+func validateChecksum(numberString string) bool {
+  parsedNumber := ""
+  total := 0
+  length := len(numberString) - 1
+  for i, j:= length, 1; i >= 0; i, j = i - 1, j + 1 {
+    parsedNumber = string(numberString[i])
+    number, _ := strconv.Atoi(parsedNumber)
+    total += number * j
+  }
+  return total % 11 == 0
+}
+
+func readFile(path string) string {
+  fileBytes, _ := ioutil.ReadFile(path)
   byteStrings := string(fileBytes)
+  return byteStrings
+}
+
+func check(err error){
+  if err != nil{
+    panic(err)
+  }
+}
+
+func getAccountsFromFile(fromFile string, toFile string) {
+  lines := getLinesFromFile()
+  result:= ""
+  accountNumber:=""
+  for i:=0; i < len(lines) - 1; i += 4 {
+    blockLines := lines[i:4 + i]
+    ocrLines := strings.Join(blockLines, "\n")
+    accountNumber = GetAccountNumber(ocrLines)
+    status := getState(accountNumber)
+    result += status
+    result += "\n"
+  }
+  WriteOnFile(result, toFile)
+}
+
+func getState(accountNumber string) string {
+  if (strings.Index(accountNumber, "?") != -1){
+    return accountNumber + " ILL"
+  }
+  if !(validateChecksum(accountNumber)){
+    alternatives, length := getAlternativeAccountNumbers(accountNumber)
+    if length > 0 {
+      if length == 1 {
+        return alternatives[0]
+      }
+      if length > 1{
+        alts := accountNumber + " AMB ["
+        for _, val := range alternatives{
+          if val != "" {
+            alts += " " 
+            alts += val
+          }
+        }
+        alts +=" ]"
+        return alts
+      }
+    }else{
+      return accountNumber + " ERR"
+    }
+  }
+  return accountNumber
+}
+
+func WriteOnFile(input string, path string){
+  byteString := []byte(input)
+  err := ioutil.WriteFile(path, byteString, 777)
+  check(err)
+}
+
+func getLinesFromFile() []string{
+  byteStrings := readFile("bankSource.txt")
   lines := strings.Split(byteStrings, "\n")
   totLines := len(lines) - 1
   return lines[0: totLines]
+}
+
+func getAlternativeAccountNumbers(accountNumber string) ([]string, int){
+  altArray := make([]string, 100)
+  key := ""
+  alternatives := []byte{}
+  j := 0
+  for i:= 0; i<len(accountNumber); i++{
+    key = string(accountNumber[i])
+    alternatives = altMap[key]
+    tmpAccountNumber := []byte(accountNumber)
+    for _, alternative := range alternatives {
+      tmpAccountNumber[i] = alternative
+      if validateChecksum(string(tmpAccountNumber)) {
+        altArray[j] = string(tmpAccountNumber)
+        j++
+      }
+      tmpAccountNumber = []byte(accountNumber)
+    }
+  }
+  return altArray, j
 }
 
--- bankocr_test.go
+++ bankocr_test.go
@@ -6,6 +6,17 @@ import(
   "strings"
 )
 
+var altMap = map[string][]byte{
+  "0" : { '8' },
+  "1" : { '7' },
+  "3" : { '9' },
+  "5" : { '6', '9' },
+  "6" : { '5','8' },
+  "7" : { '1' },
+  "8" : { '0', '6', '9' },
+  "9" : { '8', '3' },
+}
+
 var OCRMaps = map[string]string{
   " _ | ||_|": "0",
   "     |  |": "1",
@@ -19,6 +30,43 @@ var OCRMaps = map[string]string{
   " _ |_| _|": "9",
 }
 
+func ExampleStringReplace() {
+  accountNumber := "490067715"
+
+  accountByteNumber := []byte(accountNumber)
+  accountByteNumber[2] = '9'
+  fmt.Println(string(accountByteNumber))
+  // Output:
+  // 499067715
+}
+
+func ExampleGetAlternatives() {
+  accountNumber := "888888888"
+  //alternatives := []string{}
+  alternatives, _ := getAlternativeAccountNumbers(accountNumber)
+
+  //fmt.Println(status)
+  for _, alter := range alternatives {
+    fmt.Println(alter)
+  }
+
+  // Output:
+  // 888886888
+  // 888888988
+  // 888888880
+}
+
+func ExampleChecksum(){
+  invalidNumber := "111111111"
+  validNumber := "123456789"
+  fmt.Println(validateChecksum(invalidNumber))
+  fmt.Println(validateChecksum(validNumber))
+
+  // Output:
+  // false
+  // true 
+}
+
 func ExampleParseLinesIntoNumberCase1() {
   ocrnumbers := "    _  _     _  _  _  _  _ \n" +
                 "  | _| _||_||_ |_   ||_||_|\n" +
@@ -27,8 +75,6 @@ func ExampleParseLinesIntoNumberCase1() {
   // Output:
   // 123456789
 }
-
-//func ExampleValidate
 
 func ExampleReadLineOneFromFileTest() {
   lines := getLinesFromFile()
@@ -43,16 +89,51 @@ func ExampleReadLineOneFromFileTest() {
   // 123456789
   // 123456789
   // 123456789
+  // 111111111
+  // 1?1?1????
+}
+
+func ExampleConvertOCRIntoNumbers() {
+
+  getAccountsFromFile("bankSource.txt", "output.txt")
+  byteStrings := readFile("output.txt")
+  fmt.Println(byteStrings)
+  //lines := strings.Split(byteStrings, "\n")
+
+  // Output:
+  // 123456789
+  // 123456789
+  // 123456789
+  // 711111111
+  // 1?1?1???? ILL
+  //
+}
+
+func ExampleWriteOnFile() {
+  WriteOnFile("test", "output.txt")
+  byteStrings := readFile("output.txt")
+  lines := strings.Split(byteStrings, "\n")
+  fmt.Println(lines[0])
+
+  // Output:
+  // test
 }
 
 func ExampleGetLineNumbers() {
   lines := getLinesFromFile()
-  //fmt.Println(lines[11])
   totLines := len(lines)
   fmt.Println(totLines)
 
   // Output:
-  // 12
+  // 20
+}
+
+func ExampleCheckNumbersOnFile() {
+  //saveNumbersIntoFile()
+  // Output:
+  // 
+  //  ERR
+  //  ILL
 }
 
 func ExampleCharactersPerLine() {
@@ -66,9 +147,13 @@ func ExampleCharactersPerLine() {
   // 0
 }
 
-func printValue(value string) string {
-  showValue := value
-  showValue += "============="
-  return showValue
+func ExampleGetState(){
+  accountNumber := "888888888"
+  state := getState(accountNumber)
+  fmt.Println(state)
+
+  // Output:
+  // 888888888 AMB [ 888886888 888888988 888888880 ]
+
 }
 
